c1c9ff375ae85c160f0cf0a91ba7736532e8dafc
svasenkov <aanher@gmail.com>
2021-04-03 01:35:24 +0300

    fixed bug with system properties

--- src/test/java/config/TestDataConfig.java
+++ src/test/java/config/TestDataConfig.java
@@ -5,7 +5,7 @@ import org.aeonbits.owner.Config;
 @Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
         "system:properties",
-        "classpath:config/testdata.properties"
+        "classpath:config/testdata.properties",
 })
 public interface TestDataConfig extends Config {
     @Key("web.url")
